73219495cf6b739e6fd38c61962fa6859b78ca00
Sascha Eglau <sascha.eglau@gmail.com>
2013-04-03 19:05:09 +0200

    Changed to match new rzsh_path location

--- zsh/06-prompt.zsh
+++ zsh/06-prompt.zsh
@@ -5,7 +5,7 @@ local FMT_PATH="%F{1}%R%F{2}/%S%f"
 setprompt() {
   local USER="%(#.%F{1}.%F{3})%n%f"
   local HOST="%F{2}%M%f"
-  local PWD="%F{7}$(/usr/bin/rzsh_path)%f"
+  local PWD="%F{7}$($HOME/.dotfiles/bin/rzsh_path)%f"
   local TTY="%F{4}%y%f"
   local EXIT="%(?..%F{202}%?%f)"
   local PRMPT="${USER}@$HOST:${TTY}: ${PWD}
