09b4928eb243525d81afa2ab227ff8badf00c91d
Sascha Eglau <sascha.eglau@gmail.com>
2013-09-23 11:52:43 +0800

    Added missing packages

--- bin/setupEnvironment.sh
+++ bin/setupEnvironment.sh
@@ -9,7 +9,7 @@ else
 fi
 
 install_DE(){
-  package_install "xorg-server xorg-apps xorg-xinit xautolock slock awesome zsh rxvt-unicode slim ttf-dejavu"
+  package_install "xorg-server xorg-apps xorg-xinit xf86-video-vesa xautolock slock awesome zsh rxvt-unicode slim ttf-dejavu gtk-engine-murrine"
   aur_package_install "allblack-icons"
 }
 
