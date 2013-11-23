3269f58920bdf3131304929c31873a8ac4ee0b06
Sascha Eglau <sascha.eglau@gmail.com>
2013-11-23 14:19:35 +0800

    Changed aliases

--- zsh/02-aliases.zsh
+++ zsh/02-aliases.zsh
@@ -1,10 +1,9 @@
 alias de='test -f ~/.XkeymapDE && xkbcomp -w 0 ~/.XkeymapDE $DISPLAY'
 alias us='test -f ~/.XkeymapUS && xkbcomp -w 0 ~/.XkeymapUS $DISPLAY'
-alias hdmi='xrandr --output HDMI-0 --auto --left-of LVDS'
-alias pres='xrandr --fb 1366x768 --output VGA-0 --mode 1024x768 --panning 1366x0'
-alias m='xrandr --output VGA-0 --mode 1680x1050 --left-of LVDS
+alias hdmi='xrandr --output HDMI1 --auto --left-of LVDS'
+alias pres='xrandr --fb 1366x768 --output DP2 --mode 1024x768 --panning 1366x0'
+alias mon='xrandr --output DP2 --mode 1680x1050 --left-of eDP1
 feh --bg-fill $HOME/.wallpaper/current.jpg'
-alias xmind='XMind -data $HOME/.xmind/'
 alias update='sudo yaourt -Syua --noconfirm'
 alias ls='ls --color=auto'
 alias v='gvim'
@@ -24,7 +23,6 @@ alias tcmb='truecrypt --mount /dev/sdc1 $HOME/Extern2'
 alias tcub='sudo umount $HOME/Extern2'
 alias tcdb='truecrypt -d /dev/sdc1'
 alias ps='ps -a -c -o pid,command -x'
-alias picasa='wine $HOME/.wine/drive_c/Program\ Files/Google/Picasa3/Picasa3.exe'
 
 # new mount function because alias does not accept parameters
 newmount()
