ebcfe1d9677f4b4812be03e8da6045cc7ecdf03a
Sascha Eglau <sascha.eglau@gmail.com>
2014-08-02 12:55:41 +0200

    using more ohmyzsh plugins

--- zsh/lib/key-bindings.zsh
+++ zsh/lib/key-bindings.zsh
@@ -15,7 +15,7 @@ if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
   zle -N zle-line-finish
 fi
 
-bindkey -v                                            # Use emacs key bindings
+bindkey -e                                            # Use emacs key bindings
 
 bindkey '\ew' kill-region                             # [Esc-w] - Kill from the cursor to the mark
 bindkey -s '\el' 'ls\n'                               # [Esc-l] - run command: ls
