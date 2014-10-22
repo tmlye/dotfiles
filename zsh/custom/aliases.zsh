1001da88dd2ef77e405f85030d1abc49549a63d2
Sascha Eglau <se@saschaeglau.com>
2014-10-22 15:37:42 +0200

    formatted disk

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -43,7 +43,7 @@ alias um2='newumount $HOME/mount2'
 
 alias msd='sudo mount /dev/mmcblk0p1 $HOME/mount2'
 alias musb='sudo mount -U 442065E86FCD4ADE $HOME/mount2'
-alias msa='sudo mount -U 2ab49980-3dd0-4dc7-bd10-1c9a9ce0f082 .msata'
+alias msa='sudo mount -U 66a9777d-0d9e-4f18-8a31-09cd2efa4790 .msata'
 
 # Cryptsetup
 # ==========
