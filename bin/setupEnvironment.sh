57bf139945f3d923bb1c336b4d8029c3b898b408
Sascha Eglau <se@saschaeglau.com>
2020-05-15 12:15:46 +0800

    add video drivers to install script

--- bin/setupEnvironment.sh
+++ bin/setupEnvironment.sh
@@ -28,6 +28,7 @@ install_pikaur(){
 }
 
 install_desktop_environment(){
+  package_install "mesa xf86-video-amdgpu vulkan-radeon libva-mesa-driver"
   package_install "zsh rxvt-unicode wayland sway swaylock swayidle swaybg xorg-server-xwayland grim slurp ttf-dejavu gtk3"
 }
 
