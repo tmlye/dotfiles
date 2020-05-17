655b84f8062e62c7d06db0d5a9fe53553b1b15da
Sascha Eglau <se@saschaeglau.com>
2020-05-17 10:09:42 +0800

    add gnome-keyring

--- bin/setupEnvironment.sh
+++ bin/setupEnvironment.sh
@@ -29,7 +29,7 @@ install_pikaur(){
 
 install_desktop_environment(){
   package_install "mesa xf86-video-amdgpu vulkan-radeon libva-mesa-driver"
-  package_install "zsh wayland sway swaylock swayidle swaybg xorg-server-xwayland grim slurp ttf-dejavu gtk3 alacritty dmenu"
+  package_install "zsh wayland sway swaylock swayidle swaybg xorg-server-xwayland grim slurp ttf-dejavu gtk3 alacritty dmenu gnome-keyring libsecret"
 }
 
 install_communication(){
