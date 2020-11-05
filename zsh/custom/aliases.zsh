9a3a45067baa16cde20e1a7827e390fce37eff53
Sascha Eglau <se@saschaeglau.com>
2020-11-05 21:01:41 +0100

    use main instead of master in aliases

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -74,8 +74,8 @@ alias his='grephistory'
 
 alias gcb='git checkout -b'
 alias gpu='git push -u origin `git rev-parse --abbrev-ref HEAD`'
-alias gfm='git fetch origin master:master'
-alias grm='git rebase master'
+alias gfm='git fetch origin main:main'
+alias grm='git rebase main'
 alias gpl='git pull'
 
 # Kubectl
