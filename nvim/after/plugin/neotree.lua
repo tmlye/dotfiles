51a01d907a30e6f7eb950cc18d566188ec79f3d3
Sascha Eglau <se@saschaeglau.com>
2024-03-26 10:38:14 +0800

    move some plugin settings to after/plugin

---
+++ nvim/after/plugin/neotree.lua
@@ -0,0 +1,99 @@
+require("neo-tree").setup({
+  use_default_mappings = false,
+  close_if_last_window = true,
+  use_libuv_file_watcher = true,
+  follow_current_file = true,
+  sources = {
+    "filesystem",
+    "buffers",
+  },
+  window = {
+    mappings = {
+      ["o"] = "toggle_node",
+      ["<2-LeftMouse>"] = "open",
+      ["<cr>"] = "open",
+      ["S"] = "open_split",
+      ["s"] = "open_vsplit",
+      ["t"] = "open_tabnew",
+      ["w"] = "open_with_window_picker",
+      ["C"] = "close_node",
+      ["z"] = "close_all_nodes",
+      ["Z"] = "expand_all_nodes",
+      ["R"] = "refresh",
+      ["a"] = {
+        "add",
+        -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+        config = {
+          show_path = "none" -- "none", "relative", "absolute"
+        }
+      },
+      ["A"] = "add_directory", -- also accepts the config.show_path option.
+      ["d"] = "delete",
+      ["r"] = "rename",
+      ["y"] = "copy_to_clipboard",
+      ["x"] = "cut_to_clipboard",
+      ["p"] = "paste_from_clipboard",
+      ["c"] = "copy", -- takes text input for destination, also accepts the config.show_path option
+      ["m"] = "move", -- takes text input for destination, also accepts the config.show_path option
+      ["q"] = "close_window",
+      ["?"] = "show_help",
+      ["<"] = "prev_source",
+      [">"] = "next_source",
+    },
+  },
+  filesystem = {
+    follow_current_file = {
+      enabled = true
+    },
+    window = {
+      mappings = {
+        ["H"] = "toggle_hidden",
+        ["/"] = "fuzzy_finder",
+        ["D"] = "fuzzy_finder_directory",
+        ["f"] = "filter_on_submit",
+        ["<C-x>"] = "clear_filter",
+        ["<bs>"] = "navigate_up",
+        ["."] = "set_root",
+        ["[g"] = "prev_git_modified",
+        ["]g"] = "next_git_modified",
+        ["tf"] = "telescope_find",
+        ["tg"] = "telescope_grep",
+        ["o"] = "toggle_node",
+      }
+    },
+    commands = {
+      telescope_find = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        require('telescope.builtin').find_files(getTelescopeOpts(state, path))
+      end,
+      telescope_grep = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        require('telescope.builtin').live_grep(getTelescopeOpts(state, path))
+      end,
+    },
+  },
+  buffers = {
+    follow_current_file = {
+      enabled = true
+    }
+  },
+  event_handlers = {
+    {
+      event = "file_opened",
+      handler = function(file_path)
+        require("neo-tree.command").execute({ action = "close" })
+      end
+    },
+    {
+      event = "buffer_opened",
+      handler = function(file_path)
+        require("neo-tree.command").execute({ action = "close" })
+      end
+    },
+  }
+})
+
+vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>')
+vim.keymap.set('n', '<leader>b', ':Neotree buffers<CR>')
