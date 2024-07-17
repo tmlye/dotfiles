dcba79b9325f963adc19b454b8a0c3cd2aa5c229
Sascha Eglau <se@saschaeglau.com>
2024-07-17 09:41:01 +0200

    add keybind for formatting file

--- nvim/lua/custom/keymap.lua
+++ nvim/lua/custom/keymap.lua
@@ -24,6 +24,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Formatting
+vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format)
+
 -- Prevent moving cursor with space
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
