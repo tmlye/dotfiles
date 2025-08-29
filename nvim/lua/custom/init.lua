db54602a8cfd1d150d2fe034e9e34b604037de8d
Sascha Eglau <se@saschaeglau.com>
2025-08-29 20:38:35 +0200

    update nvim packages and theme

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -14,20 +14,20 @@ vim.diagnostic.config({
 })
 
 require("catppuccin").setup({
-  flavour = "mocha", -- latte, frappe, macchiato, mocha
+  flavour = "macchiato", -- latte, frappe, macchiato, mocha
   background = {     -- :h background
-    light = "latte",
-    dark = "mocha",
+    light = "frappe",
+    dark = "macchiato",
   },
   transparent_background = false, -- disables setting the background color.
   show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
   term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
   dim_inactive = {
-    enabled = false,              -- dims the background color of inactive window
+    enabled = true,               -- dims the background color of inactive window
     shade = "dark",
     percentage = 0.15,            -- percentage of the shade to apply to the inactive window
   },
-  no_italic = false,              -- Force no italic
+  no_italic = true,               -- Force no italic
   no_bold = false,                -- Force no bold
   no_underline = false,           -- Force no underline
   styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
